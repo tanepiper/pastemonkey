9edd5fd812647f277a10bed6c73be1a229219ce2
digitalspaghetti <digitalspaghetti@76ffe00d-d43a-0410-9c4e-87c6e68c8215>
2007-09-30 22:41:27 +0000

    Attempts to get Recaptcha working resulted in a half working attempt.  The standard API works fine, however the AJAX one does not.  A jQuery wrapper will need to be written for it.
    git-svn-id: http://pastemonkey.googlecode.com/svn/trunk@87 76ffe00d-d43a-0410-9c4e-87c6e68c8215

--- app/views/helpers/recaptcha.php
+++ app/views/helpers/recaptcha.php
@@ -6,9 +6,9 @@
 	
 		function render($publicKey, $error) {
 			if ($this->params['isAjax']) {
-				e('<div id="recaptcha_div"></div>');
-				e('<script type="text/javascript">document.write("http://api.recaptcha.net/js/recaptcha_ajax.js");</script>');
-				e('<script type="text/javascript">document.write("Recaptcha.create(\"' . $publicKey . '\", \"recaptcha_div\");")</script>');
+				//e('<div id="recaptcha_div"></div>');
+				//e('<script type="text/javascript">document.write("http://api.recaptcha.net/js/recaptcha_ajax.js");</script>');
+				//e('<script type="text/javascript">document.write("Recaptcha.create(\"' . $publicKey . '\", \"recaptcha_div\");")</script>');
 			} else {
 				vendor('recaptchalib');
 				return recaptcha_get_html($publicKey, $error);
