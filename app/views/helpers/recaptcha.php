64fc641d998b544f22246af0c63ab3f4cc559c84
digitalspaghetti <digitalspaghetti@76ffe00d-d43a-0410-9c4e-87c6e68c8215>
2007-09-30 22:15:03 +0000

    git-svn-id: http://pastemonkey.googlecode.com/svn/trunk@81 76ffe00d-d43a-0410-9c4e-87c6e68c8215

--- app/views/helpers/recaptcha.php
+++ app/views/helpers/recaptcha.php
@@ -1,13 +1,13 @@
 <?php
 
-	class RecaptchaHelper extends Helper {
+	class RecaptchaHelper extends JavascriptHelper {
 	
 		var $helpers = array('Javascript');
 	
 		function render($publicKey, $error) {
 			if ($this->params['isAjax']) {
-				e($javascript->link('http://api.recaptcha.net/js/recaptcha_ajax.js'));
-				e($javascript->codeBlock('Recaptcha.create(' . $publicKey . ', "recaptcha_div"'));
+				e($this->link('http://api.recaptcha.net/js/recaptcha_ajax.js'));
+				e($this->codeBlock('Recaptcha.create(' . $publicKey . ', "recaptcha_div"'));
 			} else {
 				vendor('recaptchalib');
 				return recaptcha_get_html($publicKey, $error);
