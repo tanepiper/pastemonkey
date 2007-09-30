8dd42c5ae01524e055f558ae50a3c706160706fb
digitalspaghetti <digitalspaghetti@76ffe00d-d43a-0410-9c4e-87c6e68c8215>
2007-09-30 22:12:01 +0000

    git-svn-id: http://pastemonkey.googlecode.com/svn/trunk@80 76ffe00d-d43a-0410-9c4e-87c6e68c8215

--- app/views/helpers/recaptcha.php
+++ app/views/helpers/recaptcha.php
@@ -6,8 +6,8 @@
 	
 		function render($publicKey, $error) {
 			if ($this->params['isAjax']) {
-				$javascript->link('http://api.recaptcha.net/js/recaptcha_ajax.js');
-				$javascript->codeBlock('Recaptcha.create(' . $publicKey . ', "recaptcha_div"');
+				e($javascript->link('http://api.recaptcha.net/js/recaptcha_ajax.js'));
+				e($javascript->codeBlock('Recaptcha.create(' . $publicKey . ', "recaptcha_div"'));
 			} else {
 				vendor('recaptchalib');
 				return recaptcha_get_html($publicKey, $error);
