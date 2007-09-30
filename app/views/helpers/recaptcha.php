1d0306c9901b23fd66d534565bb7f6a064875725
digitalspaghetti <digitalspaghetti@76ffe00d-d43a-0410-9c4e-87c6e68c8215>
2007-09-30 22:38:18 +0000

    git-svn-id: http://pastemonkey.googlecode.com/svn/trunk@86 76ffe00d-d43a-0410-9c4e-87c6e68c8215

--- app/views/helpers/recaptcha.php
+++ app/views/helpers/recaptcha.php
@@ -8,7 +8,7 @@
 			if ($this->params['isAjax']) {
 				e('<div id="recaptcha_div"></div>');
 				e('<script type="text/javascript">document.write("http://api.recaptcha.net/js/recaptcha_ajax.js");</script>');
-				e('<script type="text/javascript">document.write("Recaptcha.create("' . $publicKey . '", "recaptcha_div");")</script>');
+				e('<script type="text/javascript">document.write("Recaptcha.create(\"' . $publicKey . '\", \"recaptcha_div\");")</script>');
 			} else {
 				vendor('recaptchalib');
 				return recaptcha_get_html($publicKey, $error);
